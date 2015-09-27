84355770273a1144f40803b1508364e9260fe80e
Nadezhda <nemchinova_n@auca.kg>
2015-09-27 19:35:29 +0600

    Add project

---
+++ test_project/td_string_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "td_string.h"
+
+void test_td_string_create()
+{
+ 
+}
+
+void test_td_string_create_with_string()
+{
+   
+}
+
+void test_td_string_delete()
+{
+   
+}
+
+void test_td_string_get_length()
+{
+    
+}
+
+void test_td_string_combine()
+{
+    
+}
+
+void test_td_string()
+{
+    
+}
+
+int main(int argc, char **argv)
+{
+    
+}
